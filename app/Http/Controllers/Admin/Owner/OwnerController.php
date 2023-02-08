7b72b13b48fab257336bd68f8ecc0e0710aab505
young199227 <68836965+young199227@users.noreply.github.com>
2023-02-08 22:40:40 +0800

    2/8 session 測試

--- app/Http/Controllers/Admin/Owner/OwnerController.php
+++ app/Http/Controllers/Admin/Owner/OwnerController.php
@@ -7,42 +7,44 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class OwnerController extends Controller
-{   
+{
     //測試
-    public function img_test(){
+    public function test()
+    {
+        session(['user' => '123']);
 
-        $row_img = DB::table('goods_imges')->where('goods_id','37')->get();
-
-        echo $row_img;
+        session()->flush();
     }
 
     //後臺首頁(顯示所有商品)
-    public function owner_index(){
+    public function owner_index()
+    {
 
         $row = DB::table('goods')
-        ->select('*',DB::raw('(select Goods_img FROM goods_imges where Goods_id = a.Goods_id LIMIT 1) as Goods_imges'))
-        ->from('goods as a')
-        ->get();
+            ->select('*', DB::raw('(select Goods_img FROM goods_imges where Goods_id = a.Goods_id LIMIT 1) as Goods_imges'))
+            ->from('goods as a')
+            ->get();
 
-        return view('web.owner.owner_index',compact('row'));
+        return view('web.owner.owner_index', compact('row'));
     }
 
     //後臺PO商品頁面
-    public function owner_po_goods(){
+    public function owner_po_goods()
+    {
 
         return view('web.owner.owner_po_goods');
     }
 
     //後台修改商品頁面 把商品值帶過ㄑ並顯示
-    public function owner_update_goods (Request $req,$goods_id)
+    public function owner_update_goods(Request $req, $goods_id)
     {
 
-        $row = DB::table('goods')->where('goods_id',$goods_id)->first();
+        $row = DB::table('goods')->where('goods_id', $goods_id)->first();
 
-        $row_img = DB::table('goods_imges')->where('goods_id',$goods_id)->get();
+        $row_img = DB::table('goods_imges')->where('goods_id', $goods_id)->get();
 
         // return $row_img;
-        return view('web.owner.owner_update_goods', compact('row','row_img'));
+        return view('web.owner.owner_update_goods', compact('row', 'row_img'));
         // return view('web.owner.owner_update_goods',compact('row'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //測試頁面2
-Route::get('/test2',[TestController::class,'test_index']);
+Route::get('/test2', [TestController::class, 'test_index']);
 
 //測試頁面
-Route::get('/test',[OwnerController::class,'img_test']);
+Route::get('/test', [OwnerController::class, 'test']);
+Route::get('/test1',function(){return view('test');});
+
 
 //首頁(展示頁面)
-Route::get('/',[GoodsController::class,'show_index']);
+Route::get('/', [GoodsController::class, 'show_index']);
 
-//第二首頁
-Route::get('/fruit',[GoodsController::class,'fruit_index']);
+//fruit的group
+Route::group(["prefix" => "fruit"], function () {
+    //第二首頁
+    Route::get('/', [GoodsController::class, 'fruit_index']);
 
-//購物車
-Route::get('/fruit/goods_car',[GoodsController::class,'goods_car']);
+    //購物車
+    Route::get('/goods_car', [GoodsController::class, 'goods_car']);
 
-//商品列表
-Route::get('/fruit/goods_list',[GoodsController::class,'goods_list']);
+    //商品列表
+    Route::get('/goods_list', [GoodsController::class, 'goods_list']);
 
-//商品詳細
-Route::get('/fruit/goods_only/{goods_id}',[GoodsController::class,'test']);
+    //商品詳細
+    Route::get('/goods_only/{goods_id}', [GoodsController::class, 'test']);
+});
+
 
 //會員註冊
-Route::get('/member_sign_up',[MemberController::class,'member_sign_up']);
+Route::get('/member_sign_up', [MemberController::class, 'member_sign_up']);
 
-//後臺首頁(顯示所有商品)
-Route::get('/owner',[OwnerController::class,'owner_index']);
 
-//後臺owner_po_goods
-Route::get('/owner_po_goods',[OwnerController::class,'owner_po_goods']);
+//owner的group
+Route::group(["prefix" => "owner"], function () {
+    //後臺首頁(顯示所有商品)
+    Route::get('/', [OwnerController::class, 'owner_index']);
 
-//後台owner_update_goods
-Route::get('/owner/{goods_id}',[OwnerController::class,'owner_update_goods']);
+    //後臺owner_po_goods
+    Route::get('/owner_po_goods', [OwnerController::class, 'owner_po_goods']);
+
+    //後台owner_update_goods
+    Route::get('/owner_update_goods/{goods_id}', [OwnerController::class, 'owner_update_goods']);
+});
