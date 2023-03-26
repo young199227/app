4042007dc7ae383cc504c9cc4c9ac98a20d6fed0
young199227 <68836965+young199227@users.noreply.github.com>
2023-03-26 12:06:17 +0800

    3/26 實做vue 商品分頁功能

--- resources/views/test/vueTest.blade.php
+++ resources/views/test/vueTest.blade.php
@@ -20,21 +20,38 @@
 
             <div class="row">
 
-                <div class="col-3" v-for="item in ajaxData" :key="item.id">
+                <div class="col-md-3 mt-3 h-100" v-for="item in listData" :key="item">
 
                     <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
+                        <img v-bind:src="item.Goods_imges" class="card-img-top" style="width: 100%; height: 200px">
                         <div class="card-body">
-                            <h5 class="card-title">{{item.Goods_name}}</h5>
-                            <p class="card-text"></p>
-                            <a href="#" class="btn btn-primary"></a>
+                            <h5 class="card-title">名稱:{{item.Goods_name}}</h5>
+                            <p class="card-text">價錢:{{item.Goods_money}}</p>
+                            <p class="card-text">產地:{{item.Goods_area}}</p>
+                            <p class="card-text">描述:{{item.Goods_detail}}</p>
                         </div>
                     </div>
-                    
+
+                </div>
+
+                <div class="col-12 mt-3" >
+
+                    <nav aria-label="Page navigation example" >
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item"><a class="page-link" href="#" >Previous</a></li>
+
+                            <span v-for="(page, index) in ajaxData" :key="index">
+                            <li class="page-item"><a class="page-link" href="#" v-on:click="message_state(index)">{{index+1}}</a></li>
+                            </span>
+
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav>
+
                 </div>
 
             </div>
-            
+
         </div>
     </div>
     @endverbatim
@@ -47,24 +64,35 @@
     var App = {
         data() {
             return {
-                text01: '123456',
-                showMessage: true,
-                ajaxData: null
+                ajaxData: [
+                    []
+                ],
+                listData: []
             }
         },
         created() {
             axios.get('/api/goods_list_api')
                 .then(response => {
-                    this.ajaxData = response.data;
+                    console.log(response.data);
+                    // this.ajaxData[0] = response.data;
+
+                    for (i = 0; i < response.data.length; i++) {
+
+                        if (i % 4 == 0 && i != 0) {
+                            this.ajaxData.push([]);
+                        }
+
+                        this.ajaxData[this.ajaxData.length - 1][i % 4] = response.data[i];
+                    }
+                    this.listData = this.ajaxData[0];
                 })
                 .catch(error => {
                     console.error(error);
                 });
         },
         methods: {
-            message_state() {
-                this.showMessage = !this.showMessage;
-                this.text01 = "不是";
+            message_state(page) {
+                this.listData = this.ajaxData[page];
             }
 
         },
